9d69e6474a7a8c6fae7ae647f05287ee334aa632
root <cousym11>
2022-09-10 10:29:27 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,34 +4,44 @@
 
 #include <stdio.h>
 
+
+
 /**
  *
- *  * main - Prints random number to variable
+ *  * main - entry point
  *
  *   *
  *
- *    * Return: Always (Successful)
+ *    *
+ *
+ *     * Return: always return 0
  *
  *     */
 
 int main(void)
+
 {
-
 	int n;
+	int lastn;
 
-	char last[] = "Last digit of";
-        
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
+	lastn = n % 10;
 
-	printf("%s %d is %d and is ", last, n, n % 10);
-
-	if (n % 10 > 5)
-
+	if (lastn > 5)
 	{
 
-	printf("greater than 5\n");
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
+	} else if (lastn == 0)
+	{
+
+		printf("Last digit of %d is %d and is 0\n", n, lastn);
+	} else
+	{
+                        																		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
+
+	}
+
+																				return (0);
 
 }
-
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,34 +1,49 @@
+#include <stdlib.h>
+
+#include <time.h>
+
 #include <stdio.h>
 
 
 
 /**
  *
- *  * main - lowercase alphabets
+ *  * main - entry point
  *
  *   *
  *
- *    * Return: Always (Success)
+ *    *
+ *
+ *     * Return: always return 0
  *
  *     */
 
 int main(void)
-
 {
+        int n;
+        int lastn;
 
-		char c;
+        srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastn = n % 10;
 
+        if (lastn > 5)	
+	{
 
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 
-			for (c = 'a'; c <= 'z'; c++)
+	} else if (lastn == 0)	
 
-						putchar(c);
+	{
 
+		printf("Last digit of %d is %d and is 0\n", n, lastn);
 
+	} else
 
-				putchar('\n');
+													printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
 
-					return (0);
+											}
+
+         return (0);
 
 }
-
