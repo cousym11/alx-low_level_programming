339f6718f75cc68d00dbc9ac3a2a058fda8e1899
root <cousym11>
2022-10-10 05:11:52 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,91 +1,20 @@
-0x0E-structures_typedef.docx
-
-vi dog.h
-#ifndef DOG_H
-
-#define DOG_H
-
-/**
-
- * struct dog - A new type describing a dog.
-
- * @name: The name of the dog.
-
- * @age: The age of the dog.
-
- * @owner: The owner of the dog.
-
- */
-
-struct dog
-
-{
-
-        char *name;
-
-        float age;
-
-        char *owner;
-
-};
-
-
-/**
-
- * dog_t - Typedef for struct dog
-
- */
-
-typedef struct dog dog_t;
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
-
-
-#endif
-
-
-
-vi 1-init_dog.c
 #include "dog.h"
 
-#include <stdlib.h>
-
-
 /**
-
- * init_dog - Initializes a variable of type struct dog.
-
- * @d: The dog to be initialized.
-
- * @name: The name of the dog.
-
- * @age: The age of the dog.
-
- * @owner: The owner of the dog.
-
+ * init_dog - initializes a variable of type struct dog.
+ * @d: struct dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: no return.
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
-
 {
-
-        if (d != NULL)
-
-        {
-
-                d->name = name;
-
-                d->age = age;
-
-                d->owner = owner;
-
-        }
-
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
